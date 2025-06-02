27924fb9a4802dfcf70aefcab4abf022424cf911
Paula Barrionuevo <paulabc.1998@gmail.com>
2025-06-02 20:38:50 +0200

    TODO 1

--- examples/sum.test.js
+++ examples/sum.test.js
@@ -3,3 +3,12 @@ const suma = require("./sum");
 test("sumar 1 + 2 es igual a 3", () => {
   expect(suma(1, 2)).toBe(3);
 });
+
+// 0 + 0 = 0
+test.todo("Sumar 0 + 0 es igual a 0");
+
+// 0 + 1 = 1
+test.todo("Sumar 0 + 1 es igual a 1");
+
+// 1 + -3 = -2
+test.todo("Sumar 1 + -3 es igual a -2");
