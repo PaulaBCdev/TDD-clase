40431ed1c3f53a687c37aa415b302b90b4ec29b6
Paula Barrionuevo <paulabc.1998@gmail.com>
2025-06-02 20:51:32 +0200

    TODO OK + watch

--- examples/sum.test.js
+++ examples/sum.test.js
@@ -5,10 +5,16 @@ test("sumar 1 + 2 es igual a 3", () => {
 });
 
 // 0 + 0 = 0
-test.todo("Sumar 0 + 0 es igual a 0");
+test("Sumar 0 + 0 es igual a 0", () => {
+  expect(suma(0, 0)).toBe(0);
+});
 
 // 0 + 1 = 1
-test.todo("Sumar 0 + 1 es igual a 1");
+test("Sumar 0 + 1 es igual a 1", () => {
+  expect(suma(0, 1)).toBe(1);
+});
 
 // 1 + -3 = -2
-test.todo("Sumar 1 + -3 es igual a -2");
+test("Sumar 1 + -3 es igual a -2", () => {
+  expect(suma(1, -3)).toBe(-2);
+});
